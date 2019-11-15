050a3350c5e9ed69f3363d99b890f3830a693107
Luis Sanchez Alcazar <lsancheza@vectoritcgroup.com>
2019-11-15 12:39:47 +0100

    prebuild heroku

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const path = require("path");
 const express = require("express");
 
 const DIST_DIR = path.join(__dirname, "dist");
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 //Serving the files on the dist folder
 app.use(express.static(DIST_DIR));
 
-app.set("port",process.env.PORT || 3000);
+app.set("port",PORT);
 
 //Send index.html when the user access the web
 app.get("*", function (req, res) {
